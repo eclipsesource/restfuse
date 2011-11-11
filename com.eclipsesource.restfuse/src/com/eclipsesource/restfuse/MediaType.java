1d8f101d4835ebb271a69f61dc1de7290ca47cd6
hstaudacher <hstaudacher@eclipsesource.com>
2011-11-11 14:31:01 +0100

    Added maven releng stuff

--- com.eclipsesource.restfuse/src/com/eclipsesource/restfuse/MediaType.java
+++ com.eclipsesource.restfuse/src/com/eclipsesource/restfuse/MediaType.java
@@ -20,18 +20,18 @@ package com.eclipsesource.restfuse;
  */ 
 public enum MediaType {
   
-  WILDCARD ( "*/*" ),
-  APPLICATION_XML ( "application/xml" ),
-  APPLICATION_ATOM_XML ( "application/atom+xml" ),
-  APPLICATION_XHTML_XML ( "application/xhtml+xml" ),
-  APPLICATION_SVG_XML ( "application/svg+xml" ),
-  APPLICATION_JSON ( "application/json" ),
-  APPLICATION_FORM_URLENCODED ( "application/x-www-form-urlencoded" ),
-  MULTIPART_FORM_DATA ( "multipart/form-data" ),
-  APPLICATION_OCTET_STREAM ( "application/octet-stream" ),
-  TEXT_PLAIN ( "text/plain" ),
-  TEXT_XML ( "text/xml" ),
-  TEXT_HTML ( "text/html" );
+  WILDCARD( "*/*" ),
+  APPLICATION_XML( "application/xml" ),
+  APPLICATION_ATOM_XML( "application/atom+xml" ),
+  APPLICATION_XHTML_XML( "application/xhtml+xml" ),
+  APPLICATION_SVG_XML( "application/svg+xml" ),
+  APPLICATION_JSON( "application/json" ),
+  APPLICATION_FORM_URLENCODED( "application/x-www-form-urlencoded" ),
+  MULTIPART_FORM_DATA( "multipart/form-data" ),
+  APPLICATION_OCTET_STREAM( "application/octet-stream" ),
+  TEXT_PLAIN( "text/plain" ),
+  TEXT_XML( "text/xml" ),
+  TEXT_HTML( "text/html" );
 
   private final String mimeType;
   
